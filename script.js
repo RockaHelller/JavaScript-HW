9bb091342855db49d1a4098b5dded5b2043f4048
Murad Jafarov <muradjj@code.edu.az>
2023-02-04 23:17:11 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict"
+
+//#region Task1
+// let n = 42;
+
+// if (Number(a) % 3 == 0 && Number(a) % 7 == 0) {
+//     console.log("Eded 3-e ve 7-e bolunur");
+// } else{
+//     console.log("Bolunmur");
+// }
+//#endregion
+
+//#region Task2
+// let n = 6; 
+// let m = 4;
+
+// if (n % 2 == 0 && m % 2 == 0) {
+//     console.log(n + m);
+// } else{
+//     console.log("Cut olmayan eded var");
+// }
+//#endregion
+
+//#region Task3
+// let n = 4;
+// let m = 28;
+// let cnt = 0;
+
+// for (let i = n; i < m; i++) {
+//     if (i % 2 == 1) {
+//         cnt++;
+//     }
+// }
+
+// console.log(cnt);
+//#endregion
+
+//#region Task4
+// let n = 4;
+// let m = 28;
+// let cnt = 0;
+
+// for (let i = n; i < m; i++) {
+//     if (i % 2 == 1) {
+//         cnt += i;
+//     }
+// }
+
+// console.log(cnt);
+//#endregion
+
+//#region Task5
+// let n = 4;
+// let m = 28;
+// let cnt = 0;
+
+// for (let i = n; i < m; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);;
+//     }
+// }
+//#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
